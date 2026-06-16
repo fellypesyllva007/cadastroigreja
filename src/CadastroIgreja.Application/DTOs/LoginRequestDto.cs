db39fd0b396afc8d56511baabc3a56e7f6420e76
fellypesyllva007 <53619427+fellypesyllva007@users.noreply.github.com>
2026-06-16 14:37:43 -0300

    feat: adicionar dto login

---
+++ src/CadastroIgreja.Application/DTOs/LoginRequestDto.cs
@@ -0,0 +1,6 @@
+namespace CadastroIgreja.Application.DTOs;
+public class LoginRequestDto
+{
+ public string Email { get; set; } = string.Empty;
+ public string Senha { get; set; } = string.Empty;
+}
